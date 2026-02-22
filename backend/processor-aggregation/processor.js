f9cafdc5dd54ed110ff4276a367b42cc9a7b5ccc
Harshhhh123 <harshgoilkar27@gmail.com>
2026-02-22 17:15:57 +0530

    ill cry now

--- backend/processor-aggregation/processor.js
+++ backend/processor-aggregation/processor.js
@@ -59,7 +59,7 @@ async function processMessage(event) {
 
     } catch (err) {
         await client.query("ROLLBACK");
-        console.error(" Processing error present:", err);
+        console.error(" Processing error there:", err);
     } finally {
         client.release();
     }
