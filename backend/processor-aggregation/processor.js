127640cdfdb8d5aab7e94a6e67e24d4bc309b5a0
Harshhhh123 <harshgoilkar27@gmail.com>
2026-02-22 16:55:14 +0530

    none

--- backend/processor-aggregation/processor.js
+++ backend/processor-aggregation/processor.js
@@ -59,7 +59,7 @@ async function processMessage(event) {
 
     } catch (err) {
         await client.query("ROLLBACK");
-        console.error(" Processing error happening:", err);
+        console.error(" Processing error happen:", err);
     } finally {
         client.release();
     }
