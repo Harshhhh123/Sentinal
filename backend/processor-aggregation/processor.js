11c4402e9afc46811ed088ce0e08c98bd0a2a434
Harshhhh123 <harshgoilkar27@gmail.com>
2026-02-22 16:42:14 +0530

    change to test ci

--- backend/processor-aggregation/processor.js
+++ backend/processor-aggregation/processor.js
@@ -59,7 +59,7 @@ async function processMessage(event) {
 
     } catch (err) {
         await client.query("ROLLBACK");
-        console.error("❌ Processing error:", err);
+        console.error(" Processing error:", err);
     } finally {
         client.release();
     }
