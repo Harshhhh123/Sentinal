5d785d64c90b2876e80d8e9f5900693c941f8335
Harshhhh123 <harshgoilkar27@gmail.com>
2026-02-25 19:07:49 +0530

    anamoly porcessor created plys uska ci

---
+++ backend/processor-anamoly/processor.js
@@ -0,0 +1,95 @@
+const { Kafka } = require("kafkajs");
+const { Pool } = require("pg");
+require("dotenv").config();
+const kafka = new Kafka({
+    clientId: "anomaly-processor",
+    brokers: [process.env.KAFKA_BROKER]   // e.g. 172.xx.xx.xx:9092
+});
+
+const consumer = kafka.consumer({ groupId: "anomaly-group" });
+
+const pool = new Pool({
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    port: 5432
+});
+
+/* ===== EMA State Store (In-Memory) ===== */
+
+const emaState = {};   // { metricName: emaValue }
+
+const ALPHA = 0.2;     // smoothing factor
+
+/* ===== Threshold Configuration ===== */
+
+const thresholds = {
+    CPUUtilization: 20,
+    MemoryUtilization: 20,
+    RequestCount: 50,
+    ErrorCount: 5
+};
+// const thresholds = {
+//     CPUUtilization: 1,
+//     MemoryUtilization: 2,
+//     RequestCount: 5,
+//     ErrorCount: 3
+// };
+function computeEMA(metric, value) {
+    if (!emaState[metric]) {
+        emaState[metric] = value;
+        return value;
+    }
+
+    const ema = ALPHA * value + (1 - ALPHA) * emaState[metric];
+    emaState[metric] = ema;
+    return ema;
+}
+
+async function insertAnomaly(event, baseline, deviation) {
+    await pool.query(
+        `INSERT INTO anomalies 
+        (service_name, metric_name, value, baseline, deviation, timestamp)
+        VALUES ($1, $2, $3, $4, $5, $6)`,
+        [
+            event.service,
+            event.metric,
+            event.value,
+            baseline,
+            deviation,
+            event.timestamp
+        ]
+    );
+}
+
+async function run() {
+    await consumer.connect();
+    await consumer.subscribe({ topic: "metrics-topic", fromBeginning: false });
+
+    console.log("✅ Anomaly Processor Started");
+
+    await consumer.run({
+        eachMessage: async ({ message }) => {
+            const event = JSON.parse(message.value.toString());
+
+            const baseline = computeEMA(event.metric, event.value);
+            const deviation = Math.abs(event.value - baseline);
+
+            const threshold = thresholds[event.metric] || 999999;
+
+            if (deviation > threshold) {
+                console.log(`🚨 ANOMALY → ${event.metric} | Value=${event.value} | Baseline=${baseline}`);
+
+                await insertAnomaly(event, baseline, deviation);
+            } else {
+                console.log(`✔ Normal → ${event.metric} | Deviation=${deviation.toFixed(2)}`);
+            }
+        }
+    });
+}
+
+run().catch(err => {
+    console.error("❌ Processor Crash:", err);
+    process.exit(1);
+});
